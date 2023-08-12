243f475944a662f2379900acb4927e0d6d944f1a
coresecu <coresecu@proton.me>
2023-08-12 18:25:57 +0900

    Update database information

--- server/db.ts
+++ server/db.ts
@@ -1,9 +1,17 @@
 import mysql from 'mysql'
 
+// export const db = mysql.createConnection({
+//     host: "localhost",
+//     port: 3306,
+//     user: "root",
+//     password: "AkflAkfldk!@34",
+//     database: "blog"
+// })
+
 export const db = mysql.createConnection({
-    host: "localhost",
+    host: "q0h7yf5pynynaq54.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     port: 3306,
-    user: "root",
-    password: "AkflAkfldk!@34",
-    database: "blog"
+    user: "k8yetiuf157zcy8v",
+    password: "soyiz8wztq6iullo",
+    database: "vnd9zgqd290x7yvj"
 })
